b4f73bc25e9fa56627047fa4c8eb9e4ea7cff87c
RLK <rozalew123@gmail.com>
2017-11-14 00:33:49 +0100

    poprawiony routing nie kieruje na rodzica

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -18,7 +18,7 @@ import { GiftComponent } from './gift.component';
 
 
 const routes:Routes = [
-    { path: '',                                         component: ParentsComponent},
+    { path: '',                                         component: KidsComponent},
     { path: 'kids',                                     component: KidsComponent}, 
     { path: 'kids/:kidId',                              component: KidComponent},
     { path: 'kids/:kidId/new-mission',                  component: NewMissionComponent},
