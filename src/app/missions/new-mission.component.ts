2feb91d2fc1e719ec334a53bd48c184b194b284e
roselew <rozalewinska@gmail.com>
2017-11-27 15:41:29 +0100

    dodany html do dodawania misji

--- src/app/missions/new-mission.component.ts
+++ src/app/missions/new-mission.component.ts
@@ -3,12 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'new-mission',
   template: `
-    <p>
-      Wybierz misję z polecanych przez ekspertów albo...
-    </p>
+    <h1>Wybierz misje</h1>
+    <h2>polecane przez ekspertów</h2>
+    <span class="X" routerLink='../'> X </span>
+
     <expert-missions></expert-missions>
-    <button routerLink="../dodaj-wlasna">... utwórz własną</button>
-    <button routerLink='../'>Powrót</button>
+
+    <button routerLink="../dodaj-wlasna">... lub utwórz własną</button>
+
   `,
   styles: [],
 
