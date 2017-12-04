28c0db75e68be7f00b7b4357886bd59c4bd2f085
roselew <rozalewinska@gmail.com>
2017-12-04 15:44:42 +0100

    dodane przekierowanie

--- src/app/auth-kid.service.ts
+++ src/app/auth-kid.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
 export class AuthKidService implements CanActivate  {
+  
+  constructor(
+    private router: Router
+  ){}
+  
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (localStorage.getItem('loggedKid')) {return true} else {return false}
+    if (localStorage.getItem('loggedKid')) {
+      return true
+    } else {
+      this.router.navigate(['/dziecko-logowanie']);
+      return false
+    }
   }
   
 }
