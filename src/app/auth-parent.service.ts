4bab94cc386ca4c1dbf67e6ba48208ca30824d32
roselew <rozalewinska@gmail.com>
2017-12-04 15:45:30 +0100

    dodane przekierowanie

--- src/app/auth-parent.service.ts
+++ src/app/auth-parent.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable()
 export class AuthParentService implements CanActivate  {
+  
+  constructor(
+    private router: Router
+  ){}
+  
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (localStorage.getItem('loggedParent')) {return true} else {return false}
+    if (localStorage.getItem('loggedParent')) {
+      return true
+    } else {
+      this.router.navigate(['/rodzic-logowanie']);
+      return false
+    }
   }
   
 }
