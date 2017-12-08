7d7a647201c0d6746c59e209a6c3a509f82c187f
roselew <rozalewinska@gmail.com>
2017-12-08 14:49:31 +0100

    faza testowa cd.

--- src/app/progress/one-week.component.ts
+++ src/app/progress/one-week.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input} from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
 import { MissionsService } from "../missions/missions.service"
+import { UsersService } from "../session/users.service"
 
 @Component({
   selector: 'one-week',
@@ -44,6 +45,7 @@ import { MissionsService } from "../missions/missions.service"
 export class OneWeekComponent implements OnInit {
 
   constructor(
+    private users: UsersService,
     private service: MissionsServices,
     private router: Router,
     private route: ActivatedRoute, 
@@ -77,7 +79,7 @@ export class OneWeekComponent implements OnInit {
       
       //get kid Id
       if (this.mode=='kid'){
-        this.kidId = this.user.getLoggedUser('kid');
+        this.kidId = this.users.getLoggedUser('kid');
       } else {
         this.kidId = +this.route.parent.snapshot.paramMap.get('kidId');
       }
