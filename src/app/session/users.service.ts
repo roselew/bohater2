cd24d24f2e170a46afddb68d529935b8f48c02b3
roselew <rozalewinska@gmail.com>
2017-12-08 10:51:44 +0100

    nowa usługa do pobierania zalogowanych

---
+++ src/app/session/users.service.ts
@@ -0,0 +1,24 @@
+import { Injectable} from '@angular/core';
+
+@Injectable()
+export class UsersService {
+
+  constructor(    
+   ) { }
+   
+  getLoggedUser(mode){
+   if (mode=='parent') {
+     return +localStorage.getItem('loggedParent')
+   } else if (mode=='kid') {
+     return +localStorage.getItem('loggedKid')
+   } 
+
+  setLoggedUser(mode,id){
+      localStorage.clear()
+    if (mode=='parent') {
+      localStorage.setItem('loggedParent',id)
+    } else if (mode=='kid') {
+      localStorage.setItem('loggedKid',id)
+    }
+
+}
