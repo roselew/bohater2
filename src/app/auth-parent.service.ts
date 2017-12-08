6dc4f64ab51367254b159583f4f46fed107b8c81
roselew <rozalewinska@gmail.com>
2017-12-08 11:46:05 +0100

    włączona usługa users

--- src/app/auth-parent.service.ts
+++ src/app/auth-parent.service.ts
@@ -1,15 +1,17 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { UsersService } from './session/users.service';
 
 @Injectable()
 export class AuthParentService implements CanActivate  {
   
   constructor(
+    private users: UsersService,
     private router: Router
   ){}
   
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    if (localStorage.getItem('loggedParent')) {
+    if (this.users.getLoggedUser('parent')) {
       return true
     } else {
       this.router.navigate(['/rodzic-logowanie']);
