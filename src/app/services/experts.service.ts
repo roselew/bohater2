e25f5ac46ffd27ba80f4fa8fe9c4a6e6a773b2bf
roselew <rozalewinska@gmail.com>
2018-02-26 22:18:09 +0100

    poprawione ikony

--- src/app/services/experts.service.ts
+++ src/app/services/experts.service.ts
@@ -54,11 +54,11 @@ expertMissions = [ 
   {      id: 3,      name: "Posprzątać pokój",        icon: "assets/room.svg"             },    
   {      id: 4,      name: "Poskładać zabawki",       icon: "assets/ikony/m_zabawki.svg"  },    
   {      id: 5,      name: "Opróżnić zmywarkę",       icon: "assets/ikony/m_zmywarka.svg" },    
-  {      id: 6,      name: "Wstawić pranie",          icon: "assets/laundry.svg"          },    
+  {      id: 6,      name: "Wstawić pranie",          icon: "assets/ikony/m_pranie.svg"   },    
   {      id: 7,      name: "Pościelić łóżko",         icon: "assets/ikony/m_lozko.svg"    },    
   {      id: 8,      name: "Odkurzyć podłogę",        icon: "assets/ikony/m_odkurzacz.svg"},    
-  {      id: 9,      name: "Umyć podłogę",            icon: "assets/cleaning.svg"         },    
-  {      id: 10,     name: "Nakryć stół",             icon: "assets/cutlery.svg"          },    
+  {      id: 9,      name: "Umyć podłogę",            icon: "assets/ikony/m_podloga.svg"  },    
+  {      id: 10,     name: "Nakryć stół",             icon: "assets/ikony/m_nakrycie.svg" },    
   {      id: 11,     name: "Posprzątać ze stołu",     icon: "assets/table.svg"            },    
   {      id: 12,     name: "Wynieść śmieci",          icon: "assets/ikony/m_smieci.svg"   },    
   {      id: 13,     name: "Posprzątać po zwierzątku",icon: "assets/ikony/m_kuweta.svg"   },    
@@ -70,7 +70,7 @@ expertMissions = [ 
   {      id: 19,     name: "Pomoc w ogródku",         icon: "assets/ikony/m_ogrod.svg"    },    
   {      id: 20,     name: "Umyć naczynia",           icon: "assets/ikony/m_naczynia.svg" },    
   {      id: 21,     name: "Odrobić pracę domową",    icon: "assets/ikony/m_lekcje.svg"   },    
-  {      id: 22,     name: "Przeczytać książkę",      icon: "assets/book.svg"             },    
+  {      id: 22,     name: "Przeczytać książkę",      icon: "assets/ikony/m_ksiazka.svg"  },    
   {      id: 23,     name: "Poskładać ubrania",       icon: "assets/ikony/m_ubrania.svg"  },    
   {      id: 24,     name: "Ubrać się samemu",        icon: "assets/ikony/m_ubrac.svg"    }  
 ]
@@ -81,17 +81,17 @@ expertGifts = [
   {      id: 3,      name: "Wycieczka na weekend",    icon: "assets/ikony/n_wycieczka.svg"},    
   {      id: 4,      name: "Aquapark ",               icon: "assets/swimming-pool.svg"    },    
   {      id: 5,      name: "15 min na gry",           icon: "assets/ikony/n_kwadrans.svg" },    
-  {      id: 6,      name: "30 min na gry",           icon: "assets/ikony/n_pol.svg"      },    
+  {      id: 6,      name: "30 min na gry",           icon: "assets/ikony/n_pol2.svg"      },    
   {      id: 7,      name: "45 min na gry",           icon: "assets/ikony/n_trzyczwarte.svg"},    
-  {      id: 8,      name: "Nowa gra",                icon: "assets/remote-control.svg"   },    
+  {      id: 8,      name: "Nowa gra",                icon: "assets/ikony/n_pad.svg"      },    
   {      id: 9,      name: "Nowa zabawka",            icon: "assets/ikony/n_prezent.svg"  },    
   {      id: 10,     name: "Wieczór gier",            icon: "assets/ikony/n_gry.svg"      },    
   {      id: 11,     name: "Deser",                   icon: "assets/ikony/n_deser.svg"    },    
-  {      id: 12,     name: "Późniejszy powrót do domu",icon: "assets/history.svg"         },    
+  {      id: 12,     name: "Późniejszy powrót do domu",icon: "assets/ikony/n_powrot.svg"  },    
   {      id: 13,     name: "Deskorolka",              icon: "assets/ikony/n_deska.svg"    },    
-  {      id: 14,     name: "Niespodzianka",           icon: "assets/gift2.svg"            },    
+  {      id: 14,     name: "Niespodzianka",           icon: "assets/ikony/n_prezent.svg"  },    
   {      id: 15,     name: "Klocki LEGO",             icon: "assets/ikony/n_lego.svg"     },
-  {      id: 16,     name: "Wyjście na łyżwy",        icon: "assets/ice-skate.svg"        }  
+  {      id: 16,     name: "Wyjście na łyżwy",        icon: "assets/ice-skate.svg"        }
 ]
   
 expertHeroes = [
